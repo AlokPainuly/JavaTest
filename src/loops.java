f145696b44b6cd4fb9216a4f69e1a61471efb476
dell <dell@dell-PC>
2020-07-11 10:25:47 +0530

    hello

--- src/loops.java
+++ src/loops.java
@@ -1,12 +1,12 @@
 
-public class loops {public static void main(String[] args) throws Exception {
-	
- int i;
-  for(i=1;i<10;i++)
- {System.out.println("*");
-   if(i>=5)
-   break;
-}
-}
-
+public class loops {
+	public static void main(String[] args) throws Exception {
+		
+		 int i;
+		  for(i=1;i<10;i++)
+		 {System.out.println("*");
+		   if(i>=5)
+		   break;
+		}
+		}
 }
